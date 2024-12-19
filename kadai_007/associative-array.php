43589a2249b96af9cde0f72d655dd311fdd69293
ishi0909 <tororosoba41@icloud.com>
2024-12-19 11:48:54 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,17 @@
+<DOCTYPE! html>
+<html lang = "ja">
+  <head> 
+    <meta charset = "UTF-8">
+    <title>連想配列処理</title>
+  </head>
+  <body>
+    <p>
+     <?php
+      $Array = ['name' => 'onion', 'price' => '200', 'weight' => '160'];
+      print_r($Array);
+      echo '<br>';
+
+     ?>
+    </p>
+  </body> 
+</html>
